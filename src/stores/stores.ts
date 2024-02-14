46ecd432bf533094b9d8820066e53045c136ff57
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2024-02-14 13:58:09 +0100

    prepare github pages deploy

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,6 +1,4 @@
 import { derived, writable, type Readable, type Writable } from "svelte/store"
 import Epub, { Book } from "epubjs";
 
-
-
-export const library: Writable<Book[]> = writable([]);
+export const library: Writable<Book[]> = writable([Epub("https://s3.amazonaws.com/moby-dick/moby-dick.epub")]);
